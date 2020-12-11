14592a51d53bac27976d18975e3158f16afaaffb
Kosta Petan <56195950+kopetan-ms@users.noreply.github.com>
2020-12-11 18:45:03 +0100

    add pubsub example (#2)
    Co-authored-by: Kosta Petan <kosta.petan@microsoft.com>

---
+++ pubsub/publisher/Program.cs
@@ -0,0 +1,32 @@
+﻿using Dapr.Client;
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace publisher
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            var jsonOptions = new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                PropertyNameCaseInsensitive = true,
+            };
+
+            var client = new DaprClientBuilder()
+                .UseJsonSerializationOptions(jsonOptions)
+                .Build();
+
+            while (true)
+            {
+                await Task.Delay(1000);
+                var messageId = Guid.NewGuid();
+                await client.PublishEventAsync(DaprSettings.PubSubName, DaprSettings.Topic, $"{messageId}");
+                Console.WriteLine($"Message Id:{messageId}");
+            }
+        }
+    }
+}
---
+++ pubsub/subscriber/Controllers/MessagesController.cs
@@ -0,0 +1,30 @@
+﻿using Dapr;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace subscriber.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        public MessagesController(ILogger<MessagesController> logger)
+        {
+            Logger = logger;
+        }
+
+        public ILogger<MessagesController> Logger { get; }
+
+        [Topic(DaprSettings.PubSubName, DaprSettings.Topic)]
+        public async Task<ActionResult> Sub([FromBody] string messageId)
+        {
+            Logger.LogInformation($"Message Id:{messageId} received");
+            return Ok(messageId);
+        }
+    }
+}
